a1e66a49e42ad5846873970f9fc191034cb24c4d
jasonv <jdvn2517@gmail.com>
2022-05-10 17:27:36 -0700

    fixed session store post

--- react-app/src/store/session.js
+++ react-app/src/store/session.js
@@ -70,14 +70,14 @@ export const logout = () => async (dispatch) => {
 };
 
 
-export const signUp = (username, city, zipcode, email, password) => async (dispatch) => {
+export const signUp = (fullname, city, zipcode, email, password) => async (dispatch) => {
   const response = await fetch('/api/auth/signup', {
     method: 'POST',
     headers: {
       'Content-Type': 'application/json',
     },
     body: JSON.stringify({
-      username,
+      fullname,
       city,
       zipcode,
       email,
