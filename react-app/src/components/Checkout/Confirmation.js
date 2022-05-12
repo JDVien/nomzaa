e075d534066bfefecfc7146ca5e90cf3d3c2d866
jasonv <jdvn2517@gmail.com>
2022-05-12 08:37:57 -0700

    added confirmation page component, fixed logout

---
+++ react-app/src/components/Checkout/Confirmation.js
@@ -0,0 +1,22 @@
+import React from 'react';
+
+
+const Confirmation = () => {
+
+
+  return (
+    <>
+      <div className='confirmation_main'>
+        <div className='confirmation_left_column'>
+          <div id='confirmation_box'>
+            <span id='success_order_text'>Order Confirmed! Thank you for shopping with Nomzaa!</span>
+          </div>
+        </div>
+        <div className='confirmation_right_column'>
+        </div>
+      </div>
+    </>
+  )
+}
+
+export default Confirmation;
