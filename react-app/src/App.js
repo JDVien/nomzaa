2af95ecf5839c7bef81977bcf4ed17321f889be0
jasonv <jdvn2517@gmail.com>
2022-05-13 18:48:13 -0700

    review delete functionality implemented

--- react-app/src/App.js
+++ react-app/src/App.js
@@ -14,6 +14,7 @@ import Main from './components/MainPage/index';
 import OrderList from './components/Orders/OrderList';
 import CreateReview from './components/Reviews/CreateReviewForm';
 import EditReview from './components/Reviews/EditReviewForm';
+import DeleteReview from './components/Reviews/DeleteReview';
 import { authenticate } from './store/session';
 
 function App() {
@@ -54,6 +55,9 @@ function App() {
         </ProtectedRoute>
         <ProtectedRoute path="/reviews/:reviewId/edit">
             <EditReview />
+        </ProtectedRoute>
+        <ProtectedRoute path="/reviews/:reviewId/delete">
+            <DeleteReview />
         </ProtectedRoute>
         <ProtectedRoute exact path="/" >
           <Main user={user} />
