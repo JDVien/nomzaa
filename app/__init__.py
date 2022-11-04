0ce703b7d293e402c5f3b531a044c8e8c612610f
jasonv <jdvn2517@gmail.com>
2022-11-04 14:31:46 -0700

    render deployment

--- app/__init__.py
+++ app/__init__.py
@@ -17,7 +17,10 @@ from .seeds import seed_commands
 
 from .config import Config
 
-app = Flask(__name__)
+# app = Flask(__name__)
+
+# adjust for render deployment:
+app = Flask(__name__, static_folder='../react-app/build', static_url_path='/')
 
 # Setup login manager
 login = LoginManager(app)
